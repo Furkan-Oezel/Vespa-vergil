5d70db26c71ce0e7cdde4bb770cc7785d591940e
Furkan Özel <work.kan314@gmail.com>
2024-04-04 17:36:39 +0200

    feat: add bpf program that prints a debug message when using the command chmod

---
+++ chmod.c
@@ -0,0 +1,12 @@
+
+// go:build ignore
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+SEC("lsm/path_chmod")
+int BPF_PROG(path_chmod, const struct path *path, umode_t mode) {
+  bpf_printk("hi %s\n, path->dentry->d_name");
+  return 0;
+}
