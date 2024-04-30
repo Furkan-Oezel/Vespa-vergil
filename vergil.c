c9a121d8cd01e5f82deefdc937361119a1ce7f08
Furkan Özel <work.kan314@gmail.com>
2024-04-30 16:21:54 +0200

    forbid any chmod operation in /home/furkan

--- vergil.c
+++ vergil.c
@@ -4,10 +4,21 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <linux/errno.h>
+#include <string.h>
 
 SEC("lsm/path_chmod")
 int BPF_PROG(path_chmod, const struct path *path, umode_t mode) {
-  bpf_printk("hi %s\n, path->dentry->d_name");
+  bpf_printk("hi\n");
+
+  char buf[32];
+  bpf_probe_read_str(buf, sizeof(buf), path->dentry->d_parent->d_name.name);
+
+  if (strncmp(buf, "furkan", 6) == 0) {
+    bpf_printk("chmod attempted in %s\n", buf);
+    return -EPERM;
+  }
+
   return 0;
 }
 
