371bf73393ec4c7efbc16c2a6761b77e70bf3fb6
Furkan Özel <work.kan314@gmail.com>
2024-10-17 12:59:27 +0200

    rearrange: files deleted

--- collect_docker_info_directory/gen.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go collect_docker_info collect_docker_info.c
--- map_directory/gen.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go docker_process_IDs_map docker_process_IDs_map.c
